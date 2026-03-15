7b3e31eb911032862bbda980229260e59dd3961b
jeremyecker <jeremy@resteasypest.com>
2026-03-15 16:13:42 -0400

    Fix blog image: add images.pexels.com to Next.js image domains

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'images.pexels.com' },
     ],
   },
 };
